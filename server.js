b6af9cc7abca04931346d1bb107d356f2d67ea07
manjeete <manjeetkr2017@gmail.com>
2021-10-29 14:52:37 +0530

    testing

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const room_id = 123;
+io.on('connection', (socket) => {
+    console.log("user connected")
+    socket.on(`server ${room_id}`, (msg) => {
+      io.emit(`receiver ${room_id}`, msg);
+    });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
