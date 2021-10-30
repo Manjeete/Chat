a0328e7c5fbda91a7239b07cf39e968d6bee2d29
manjeete <manjeetkr2017@gmail.com>
2021-10-30 17:21:15 +0530

    testing

--- server.js
+++ server.js
@@ -5,15 +5,21 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.get("/",(req,res) =>{
+    res.status(200).json({
+        status:true,
+        msg:"Hello"
+    })
+})
 
-const room_id = 123;
-io.on('connection', (socket) => {
-    console.log("user connected")
-    socket.on(`server ${room_id}`, (msg) => {
-      io.emit(`receiver ${room_id}`, msg);
+app.get('/chat', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+    const room_id = 123;
+    io.on('connection', (socket) => {
+        console.log("user connected")
+        socket.on(`server ${room_id}`, (msg) => {
+        io.emit(`receiver ${room_id}`, msg);
+        });
     });
 });
 
